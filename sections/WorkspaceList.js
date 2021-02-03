b7e26547a121bd6cba51db4dab3febd1fcef53ab
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-03 14:09:00 -0800

    add a workspace list

--- sections/WorkspaceList.js
+++ sections/WorkspaceList.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import WorkspaceItem from '../components/WorkspaceItem';
 
 const WorkspaceList = () => {
-    return (
-        
-    );
+  const workspaces = useSelector((s) => s.user.workspaces);
+  return (
+    <div>
+      {workspaces.map((v) => (
+        <WorkspaceItem
+          key={v._id}
+          companyName={v.entity.name}
+          industry={v.entity.domain}
+          description={'This is a company!'}
+          componentSum={v.components.length}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default WorkspaceList;
