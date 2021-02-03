034fcc9e8b871a1c6da586fa0b2176518193f17e
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-02 21:17:40 -0800

    add new boilerplate for redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
-import React from 'react';
+// Redux/persist dependencies
+// import { useStore } from '../redux/store'
+import { useStore } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from 'next/app';
 
 import '../.semantic/dist/semantic.min.css';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
-  }
+import { wrapper } from '../redux/clientStore';
+
+function MyApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+  return (
+    // <Provider store={store}>
+    <PersistGate loading={<div>loading</div>} persistor={store.__persistor}>
+      <Component {...pageProps} />
+    </PersistGate>
+    // </Provider>
+  );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
