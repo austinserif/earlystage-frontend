9ec27f73ccf5af50c1fbcd0b3bcb39b811ac3fd6
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-02 21:17:20 -0800

    add hook for question answer

---
+++ hooks/useQuestionAnswer.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const useQuestionAnswerComponent = () => {
+  const [question, setQuestion] = useState();
+  const [answer, setAnswer] = useState();
+};
+
+export default useQuestionAnswerComponent;
