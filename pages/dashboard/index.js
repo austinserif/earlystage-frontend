90bbdbec49967dc5c24c4384f825a2fcf67637a0
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-03 13:00:45 -0800

    changed dashboard component

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,23 +1,26 @@
-import { Button } from 'semantic-ui-react';
+import { Button, Container, Header } from 'semantic-ui-react';
 import { useDispatch } from 'react-redux';
 import { useRouter } from 'next/dist/client/router';
 import { clearUser } from '../../redux/auth/authActionCreators';
 import { getUserData } from '../../api/user';
 import DashboardMenu from '../../components/DashboardMenu';
+import DashboardNavbar from '../../sections/DashboardNavbar';
+import DashboardHeader from '../../components/DashboardHeading';
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   const dispatch = useDispatch();
   const router = useRouter();
+  console.log(props.name);
   return (
     <>
-      <DashboardMenu />
-      <Button
-        onClick={() => {
-          dispatch(clearUser());
-          router.push('/');
-        }}>
-        Log out
-      </Button>
+      <Container>
+        <DashboardNavbar />
+      </Container>
+
+      <Container text style={{ marginTop: '7em' }}>
+        <DashboardHeader title="Dashboard" />
+        <WorkspaceList />
+      </Container>
     </>
   );
 };
@@ -25,7 +28,9 @@ const Dashboard = () => {
 export const getStaticProps = async () => {
   try {
     const response = await getUserData();
+    console.log(response);
     const { name, isVerified, workspaces, questions, metadata } = response;
+    console.log(name);
 
     return {
       props: {
@@ -38,6 +43,7 @@ export const getStaticProps = async () => {
     };
   } catch (err) {
     // an error has occured
+    console.log(err);
     return {
       props: {
         autoLogOut: true
