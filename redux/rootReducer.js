8c73c5fcf29cbbba211234dba0a6cf6f0468018c
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-03 18:28:20 -0800

    added user reducer to root

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,11 +1,11 @@
 // `rootReducer.js` combines auth and user reducers into the root
-// import userReducer from './user/userReducer';
+import userReducer from './user/userReducer';
 import authReducer from './auth/authReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  auth: authReducer
-  // user: userReducer
+  auth: authReducer,
+  user: userReducer
 });
 
 export default rootReducer;
