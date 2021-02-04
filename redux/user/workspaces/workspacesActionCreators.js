872dd69ed8de17b5cacdc516aa39f799f984819c
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-03 18:30:07 -0800

    fixed bug in worksapces action creators

--- redux/user/workspaces/workspacesActionCreators.js
+++ redux/user/workspaces/workspacesActionCreators.js
@@ -1,13 +1,13 @@
 import * as types from './workspacesActionTypes';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
-const SERVER_URL = process.env.SERVER_URL;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 /**
  * Writes bulk workspace data into state
  * @param {Object} workspaces
  */
-const setWorkspaces = (workspaces) => ({
+export const setWorkspaces = (workspaces) => ({
   type: types.SET_WORKSPACES,
   payload: {
     workspaces
@@ -18,36 +18,51 @@ const setWorkspaces = (workspaces) => ({
  * Sets loading flag to true
  */
 const setIsLoading = () => ({
-  type: types.SET_WORKSPACES_IS_LOADING
+  type: types.SET_WORKSPACE_IS_LOADING
 });
 
 /**
  * Sets loading flag in state back to false
  */
 const clearIsLoading = () => ({
-  type: types.CLEAR_WORKSPACES_IS_LOADING
+  type: types.CLEAR_WORKSPACE_IS_LOADING
 });
 
+/**
+ *
+ * @param {String} email
+ * @param {String} token
+ * @param {String[]} workspaceIdArray
+ */
 export const getWorkspaces = (email, token, workspaceIdArray) => async (dispatch) => {
   try {
-    const dispatch = useDispatch();
-
+    // initiate loading
     dispatch(setIsLoading());
 
-    const response = await axios.all([
-      workspaceIdArray.map((v) =>
-        axios.get(`${SERVER_URL}/users/${email}/workspaces/${v}?_token=${token}`)
-      )
-    ]);
+    console.log(email, token, workspaceIdArray);
+
+    const getRequest = (url, email, workspaceId, token) =>
+      axios.get(`${url}/users/${email}/workspaces/${workspaceId}?_token=${token}`);
+
+    // request data for all workspace ids
+    const responses = await axios.all(
+      workspaceIdArray.map((v) => getRequest(SERVER_URL, email, v, token))
+    );
+
+    console.log(responses);
 
     // map response data array into a workspace object
     const workspaces = {};
 
-    response.forEach((v) => {
-      const id = v.data._id;
-      const res = v.data;
-      delete res[v.data];
-      workspaces[id] = res;
+    responses.forEach((v) => {
+      const { data } = v; // destructure data object from response
+      const id = data._id; // isolate id
+      delete data._id; // delete id from original object
+
+      // set key-value pair into workspaces object, where the
+      // key is the workspaceId, and the value is the rest of
+      // the original data response object
+      workspaces[id] = data;
     });
 
     // dispatch workspace object
