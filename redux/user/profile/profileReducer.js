de1a081b3e31ddd18e83bb1f4bafb4a60241f240
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-04 22:17:27 -0800

    added more initial states

--- redux/user/profile/profileReducer.js
+++ redux/user/profile/profileReducer.js
@@ -1,14 +1,15 @@
 import * as types from './profileActionTypes';
 
 const initialState = {
-  _id: null
+  _id: null,
+  email: '',
+  name: ''
 };
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.SET_USER_PROFILE:
       return {
-        ...state,
         ...action.payload
       };
     case types.CLEAR_USER_PROFILE:
