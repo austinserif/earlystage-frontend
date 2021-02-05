407b2c728b9cb617aa35b67741c6e18ea1a9233c
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-04 22:18:43 -0800

    modified new workspace modal

--- sections/NewWorkspaceModal.js
+++ sections/NewWorkspaceModal.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Button, Input, Form, Message } from 'semantic-ui-react';
-import useLogin from '../hooks/useLogin';
-import { emailRegex } from '../utils/regex';
+import React from 'react';
+import { Modal, Button, Input, Form, Message, Icon } from 'semantic-ui-react';
+import useNewWorkspace from '../hooks/useNewWorkspace';
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/dist/client/router';
 
-const NewWorkspaceModal = ({ open, setOpen, inverted }) => {
-
-  const [workspaceName, setWorkspaceName] = useState("");
+const NewWorkspaceModal = ({ open, setOpen }) => {
+  const { isLoading, newWorkspaceErrMsg } = useSelector((s) => s.user.workspaces);
+  const [values, handleChange, handleReset, handleSubmit] = useNewWorkspace();
   return (
     <Modal
       closeOnEscape={false}
@@ -16,22 +14,42 @@ const NewWorkspaceModal = ({ open, setOpen, inverted }) => {
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
       open={open}
-      trigger={<Button inverted={inverted}>Login</Button>}>
-      <Modal.Header>Login</Modal.Header>
+      trigger={
+        <Button animated color="green">
+          <Button.Content visible>New Workspace</Button.Content>
+          <Button.Content hidden>
+            <Icon name="add" />
+          </Button.Content>
+        </Button>
+      }>
+      <Modal.Header>New Company Workspace</Modal.Header>
       <Modal.Content>
-        <Form loading={isLoading} error={!authErrorMessage ? false : true} onSubmit={handleSubmit}>
+        <Form
+          loading={isLoading}
+          error={!newWorkspaceErrMsg ? false : true}
+          onSubmit={handleSubmit}>
           <Form.Field
             id="form-input-control-name"
             control={Input}
             fluid
             label="Company Name"
-            name="company"
-            value={email}
-            placeholder="Email"
+            name="name"
+            value={values.name}
+            placeholder="Name"
             onChange={handleChange}
           />
-          {authErrorMessage ? (
-            <Message error header="Error" content={authErrorMessage || ''} />
+          <Form.Field
+            id="form-input-control-domain"
+            control={Input}
+            fluid
+            label="Domain or Web Address"
+            name="domain"
+            value={values.domain}
+            placeholder="Domain"
+            onChange={handleChange}
+          />
+          {newWorkspaceErrMsg ? (
+            <Message error header="Error" content={newWorkspaceErrMsg || ''} />
           ) : null}
         </Form>
       </Modal.Content>
@@ -45,7 +63,7 @@ const NewWorkspaceModal = ({ open, setOpen, inverted }) => {
           }}
         />
         <Button
-          disabled={isLoading || !values.email.match(emailRegex) || values.password.length < 7}
+          disabled={isLoading || values.name.length < 1 || values.domain.length < 4}
           content="Submit"
           labelPosition="right"
           icon="checkmark"
