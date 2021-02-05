1344f508efab5b66e66c24f8b8a300f6813f1b4c
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-04 17:01:43 -0800

    mod change sign up

--- hooks/useSignUp.js
+++ hooks/useSignUp.js
@@ -1,60 +1,36 @@
 import { useState } from 'react';
-import Axios from 'axios';
-import { SERVER_URL } from '../config';
+import { useDispatch } from 'react-redux';
+import { attemptRegistration, clearAuthErrorMsg } from '../redux/auth/authActionCreators';
 
 const useSignUp = () => {
+  // set initial values as empty strings
   const initial = {
     name: '',
     email: '',
     password: ''
   };
 
+  const dispatch = useDispatch();
+
+  // values
   const [values, setValues] = useState(initial);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setValues(() => ({ ...values, [name]: value }));
   };
 
-  const handleSubmit = async () => {
-    try {
-      setIsLoading(() => true);
-      // destructure target values from state
-      const { email, password } = values;
-
-      // make login request to the server
-      const response = await Axios({
-        method: 'post',
-        url: `${SERVER_URL}/users`,
-        data: {
-          name,
-          email,
-          password
-        }
-      });
-
-      console.log(response);
-
-      // set jwt into state
-    } catch (err) {
-      // set the current error messsage into state and display
-      setErr(() => err.response.data.message || 'There was an error');
-    } finally {
-      setIsLoading(() => false);
-    }
+  const handleSubmit = () => {
+    const { email, password } = values;
+    dispatch(attemptRegistration(name, email, password));
   };
 
   const handleReset = () => {
-    // only allow reload if user
-    if (!isLoading) {
-      setValues(() => ({ ...initial }));
-      setErr(() => false);
-    }
+    setValues(() => initial);
+    dispatch(clearAuthErrorMsg());
   };
 
-  return [values, err, handleChange, handleReset, handleSubmit, isLoading];
+  return [values, handleChange, handleReset, handleSubmit];
 };
 
 export default useSignUp;
