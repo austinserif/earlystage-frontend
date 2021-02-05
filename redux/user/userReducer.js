456782c2eff3910831318d30c63bbcfbc66ee7e7
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-04 22:17:14 -0800

    added profile reducer to userReducer comb

--- redux/user/userReducer.js
+++ redux/user/userReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import workspacesReducer from './workspaces/workspacesReducer';
+import profileReducer from './profile/profileReducer';
 
 const userReducer = combineReducers({
-  workspaces: workspacesReducer
+  workspaces: workspacesReducer,
+  profile: profileReducer
 });
 
 export default userReducer;
