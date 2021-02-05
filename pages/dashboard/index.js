55e73dd3334b44e4fcb3945076feb701e8179486
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-04 20:27:09 -0800

    deleted extra imports from file

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,5 @@
-import { Button, Container, Header } from 'semantic-ui-react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useRouter } from 'next/dist/client/router';
-import { clearUser } from '../../redux/auth/authActionCreators';
-import { getUserData } from '../../api/user';
-import DashboardMenu from '../../components/DashboardMenu';
+import { Container } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
 import DashboardNavbar from '../../sections/DashboardNavbar';
 import DashboardHeader from '../../components/DashboardHeading';
 import WorkspaceList from '../../sections/WorkspaceList';
@@ -24,6 +20,5 @@ const Dashboard = () => {
     </>
   );
 };
-
 
 export default Dashboard;
