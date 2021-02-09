1b6d823422d6f707863158acc19011d8275d5cc7
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-08 17:02:38 -0800

    added docs and loading to workspacelist

--- sections/WorkspaceList.js
+++ sections/WorkspaceList.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import WorkspaceItem from '../components/WorkspaceItem';
 import { Card } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const WorkspaceList = ({ workspaceArray }) => {
+/**
+ * WorkspaceList takes an array of workspaceIds as an argument,
+ * and maps each item into a component to initiate data loading.
+ * @param {*} param0
+ */
+const WorkspaceList = ({ workspaceArray, workspaces }) => {
   // map the input prop `workspaceArray` into an array of JSX `WorkspaceItem` compaonents
-  const components = workspaceArray.map((item) => {
+  const components = workspaces.workspaces.map((item) => {
     const [k, v] = item;
     return (
       <WorkspaceItem
         key={k}
-        companyName={v.entity.name}
-        industry={v.entity === undefined ? '' : v.entity.domain}
-        componentSum={v.components ? v.components.length : 0}
+        workspaceName={v.entity.name}
       />
     );
   });
+
+  if (!workspaceArray || !workspaceArray.length) {
+    return <div>To add a workspace, select New Workspace</div>;
+  }
+
+  if (workspaces.isLoading) {
+    return (<div>...loading</div>)
+  }
 
   // render list of components if any exist
   return <Card.Group>{components}</Card.Group>;
 };
 
-export default WorkspaceList;
+export default connect((state) => state.user.workspaces)(WorkspaceList);
