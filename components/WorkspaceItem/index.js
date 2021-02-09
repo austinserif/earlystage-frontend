988f87b97a57c83a09ddfec69c514600d262d40e
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-08 16:36:15 -0800

    added loading trigger to card

--- components/WorkspaceItem/index.js
+++ components/WorkspaceItem/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Card, Icon, Button } from 'semantic-ui-react';
+import { Card, Icon, Button, Loader, Placeholder } from 'semantic-ui-react';
+import useSWR from 'swr';
+import cookieCutter from 'cookie-cutter';
+import { connect, useSelector } from 'react-redux';
 
-const WorkspaceItem = ({ companyName, industry, componentSum }) => (
-  <Card
-    header={companyName}
-    meta={industry}
-    extra={
-      <>
-        <a>
-          <Icon name="tasks" />
-          {`${componentSum}`} Components
-        </a>
-        <Button floated="right" color="blue">
-          Edit
-        </Button>
-      </>
-    }
-  />
-);
-export default WorkspaceItem;
+const WorkspaceItem = ({ workspaceId }) => {
+  const workspace = useSelector((s) => s.user.workspaces.workspaces[workspaceId]);
+
+  if (!workspace || workspace.isLoading) {
+    return (
+      <Card>
+        <Placeholder.Header image>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Card>
+    );
+  }
+
+  return (
+    <Card
+      header={workspace.entity.name}
+      meta={workspace.entity.domain}
+      extra={
+        <>
+          <a>
+            <Icon name="tasks" />
+            {workspace.components.length} Components
+          </a>
+          <Button floated="right" color="blue">
+            Edit
+          </Button>
+        </>
+      }
+    />
+  );
+};
+
+export default connect((state) => state.user.workspaces.workspaces)(WorkspaceItem);
