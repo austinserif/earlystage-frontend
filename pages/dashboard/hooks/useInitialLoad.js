af95d96edd92a84d62bb9164c9f072144238c938
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-25 15:37:04 -0800

    move parseCookies func to utils and import

--- pages/dashboard/hooks/useInitialLoad.js
+++ pages/dashboard/hooks/useInitialLoad.js
@@ -1,26 +1,9 @@
 import useSWR from 'swr';
 import fetcher from '../../../utils/fetcher';
 import { useSelector, useDispatch } from 'react-redux';
-import cookieCutter from 'cookie-cutter';
 import mapData from '../../../utils/mapData';
 import { setWorkspaces } from '../../../redux/user/workspaces/workspacesActionCreators';
-
-const parseCookies = (...args) => {
-  // instantiates a new object
-  const cookiesObject = new Object();
-
-  // for each valid string input that has a valid cookie value, add a key value pair to the object and return it
-  args.forEach((v) => {
-    if (typeof v === 'string' || v instanceof String) {
-      const cookieValue = cookieCutter.get(v);
-      if (typeof cookieValue === 'string' || cookieValue instanceof String) {
-        cookiesObject[v] = cookieValue;
-      }
-    }
-  });
-
-  return cookiesObject;
-};
+import parseCookies from '../../../utils/parseCookies';
 
 const useInitialLoad = () => {
   // retrieves status of current user cache
@@ -51,7 +34,7 @@ const useInitialLoad = () => {
     dispatch(setWorkspaces(mappedWorkspaces));
 
     // get current components data for workspaces
-    const { data: componentsData, error: componentsError } = useSWR([`/users/${cookies.email}/workspaces/${}`], fetcher);
+    const { data: componentsData, error: componentsError } = useSWR(userData ? [`/users/${cookies.email}/workspaces/${}/components?_token=${cookies.token}`], fetcher);
     const isLoadingComponents = !componentsData && !componentsError;
 
     return {
