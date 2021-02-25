d0bb1c5db1522cc315013454ab1e5eb2a0da667a
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-25 10:37:30 -0800

    credentials now passed as args, not through useSelector

--- hooks/useNewWorkspace.js
+++ hooks/useNewWorkspace.js
@@ -2,10 +2,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNewWorkspace } from '../redux/user/workspaces/workspacesActionCreators';
 
-const useNewWorkspace = () => {
-  const email = useSelector((s) => s.user.profile.email);
-  const token = useSelector((s) => s.auth.token);
-
+const useNewWorkspace = (credentials) => {
   // set initial values as empty strings
   const initial = {
     name: '',
@@ -26,9 +23,6 @@ const useNewWorkspace = () => {
   const handleSubmit = () => {
     // name and domain of new company workspace to be created
     const { name, domain } = values;
-
-    // email and token packaged into a credentials object to be used to authenticate the requesting user
-    const credentials = { email, token };
 
     // dispatch credentials, name and domain
     dispatch(createNewWorkspace(name, domain, credentials));
