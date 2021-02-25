f198fc8ba179b2f21b4488ab2f427cd3fad809cd
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-24 23:49:50 -0800

    added useNewQuestion hook

---
+++ components/NewQuestionForm/useNewQuestion.js
@@ -0,0 +1,47 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { newQuestion } from '../../redux/user/questions/questionsActionCreators';
+
+const useNewQuestion = ({ email, token }) => {
+  const dispatch = useDispatch();
+
+  // hard coded categories (for now)
+  const categories = [
+    { key: 'financials', text: 'Financials', value: 'financials' },
+    { key: 'corporateGovernance', text: 'Corporate Governance', value: 'corporateGovernance' },
+    { key: 'market', text: 'Market', value: 'market' },
+    { key: 'management', text: 'Management', value: 'management' },
+    { key: 'other', text: 'Other', value: 'other' }
+  ];
+
+  // state initializations
+  const [question, setQuestion] = useState(''); // question input controlled value
+  const [category, setCategory] = useState(''); // category controlled value
+  // const [answer, setAnswer] = useState(''); // answer controlled value
+
+  const handleChangeInput = (e) => setQuestion(() => e.target.value);
+
+  const handleChangeSelect = (e, data) => {
+    console.log(data);
+    setCategory(() => data.value);
+  };
+
+  const handleSubmit = () => {
+    const answerType = null; // this is set to null until feature implemented
+    dispatch(newQuestion(category, question, answerType, { email, token }));
+  };
+
+  const handleReset = () => {
+    setQuestion('');
+    setCategory('');
+  };
+
+  return [
+    question,
+    category,
+    categories,
+    { handleChangeInput, handleChangeSelect, handleSubmit, handleReset }
+  ];
+};
+
+export default useNewQuestion;
