c4777e6cbb5268072f6aeff453f8f581db0b8b43
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-25 10:35:55 -0800

    added workspace Item

--- components/WorkspaceItem/index.js
+++ components/WorkspaceItem/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List, Item } from 'semantic-ui-react';
 import veronika from '../../assets/img/veronika.jpg';
 import Link from 'next/link';
+import WorkspacePortal from '../../sections/WorkspacePortal';
 
 const WorkspaceItem = ({ workspaceName, workspaceId, workspaceComponentsCount }) => {
+  const [selected, setSelected] = useState(false);
   return (
     <Link href={`/workspace/${workspaceId}`}>
-      <List.Item id={workspaceId} style={{ padding: '1em 0 1em 0' }}>
+      <List.Item
+        id={workspaceId}
+        style={{
+          padding: '1em 1em 1em 1em',
+          backgroundColor: selected ? '#EEEEEE' : 'transparent',
+          cursor: 'pointer'
+        }}
+        onMouseEnter={() => setSelected(true)}
+        onMouseLeave={() => setSelected(false)}>
         <Item.Image src={veronika} />
         <Item.Content
           verticalAlign="top"
           header={workspaceName}
           meta={`${workspaceComponentsCount} Components`}
         />
+        {/* <WorkspacePortal workspaceName={workspaceName} workspaceId={workspaceId} /> */}
       </List.Item>
     </Link>
   );
