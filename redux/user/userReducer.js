af806d8f0a3e715d29ddcfccdfc673690cbd75b8
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-25 12:00:21 -0800

    add questions reducer to user redcuer

--- redux/user/userReducer.js
+++ redux/user/userReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import workspacesReducer from './workspaces/workspacesReducer';
 import profileReducer from './profile/profileReducer';
+import questionsReducer from './questions/questionsReducer';
 
 const userReducer = combineReducers({
   workspaces: workspacesReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  questions: questionsReducer
 });
 
 export default userReducer;
