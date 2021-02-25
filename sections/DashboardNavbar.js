09b9c483b9fb93a08e99deb8fab24d4e9c3137e5
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-02-25 12:02:37 -0800

    modify dashboard navbar

--- sections/DashboardNavbar.js
+++ sections/DashboardNavbar.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Container, Menu, Icon, Button } from 'semantic-ui-react';
+import { Container, Menu, Button, Image } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { flushUserDataCache } from '../redux/auth/authActionCreators';
+import { logoutAndClearCache } from '../redux/auth/authActionCreators';
 import { useRouter } from 'next/dist/client/router';
 import cookieCutter from 'cookie-cutter';
+import logo from '../assets/img/logo.svg';
 
 const ButtonContainer = styled.div`
   margin: 5px;
@@ -23,23 +24,23 @@ const DashboardNavbar = () => {
       <Container text>
         <Menu.Item as="a" onClick={() => setActiveItem('Dashboard')}>
           <ButtonContainer>
-            <Icon name="home" size="large" />
+            <Image alt="" src={logo} />
           </ButtonContainer>
         </Menu.Item>
 
         <Menu.Menu position="right">
-          <Menu.Item
+          {/* <Menu.Item
             name="Dashboard"
             active={activeItem === 'Dashboard'}
             onClick={handleItemClick}
           />
           <Menu.Item name="Profile" active={activeItem === 'Profile'} onClick={handleItemClick} />
-          <Menu.Item name="Settings" active={activeItem === 'Settings'} onClick={handleItemClick} />
+          <Menu.Item name="Settings" active={activeItem === 'Settings'} onClick={handleItemClick} /> */}
           <Menu.Item>
             <ButtonContainer>
               <Button
                 onClick={() => {
-                  // dispatch(flushUserDataCache());
+                  dispatch(logoutAndClearCache());
                   cookieCutter.set('token', '', { expires: new Date(0) });
                   cookieCutter.set('email', '', { expires: new Date(0) });
                   cookieCutter.set('isVerified', '', { expires: new Date(0) });
@@ -55,4 +56,4 @@ const DashboardNavbar = () => {
   );
 };
 
-export default DashboardNavbar;
+export default React.memo(DashboardNavbar);
