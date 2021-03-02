4a89bdd34d4487d496861fa8a97b47d75fd0050d
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-01 21:42:59 -0800

    function returns {} when no value is retrieved

--- utils/questionSearchFunction.js
+++ utils/questionSearchFunction.js
@@ -5,11 +5,11 @@
  * NOTE: This is a temporary workaround
  */
 export default function questionSearchFunction(qid, object) {
-  console.log(object);
   const questionsByCategory = Object.values(object); // creates an array of objects
-  console.log(questionsByCategory);
   const allQuestions = Object.assign({}, ...questionsByCategory);
-  console.log(allQuestions);
-  console.log('------- finished ---------');
-  return allQuestions[qid];
+  if (allQuestions[qid] === undefined) {
+    return {};
+  } else {
+    return allQuestions[qid];
+  }
 }
