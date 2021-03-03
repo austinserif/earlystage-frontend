d2378f10357adc15447753143fad614a795ed576
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-03 11:58:20 -0800

    added delete button to workspace item

--- components/WorkspaceItem/index.js
+++ components/WorkspaceItem/index.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
-import { List, Item, Card } from 'semantic-ui-react';
+import { List, Item, Card, Button, Segment, Icon } from 'semantic-ui-react';
 import veronika from '../../assets/img/veronika.jpg';
 import Link from 'next/link';
 import WorkspacePortal from '../../sections/WorkspacePortal';
+import DeleteButton from '../DeleteButton';
+import { deleteWorkspaceAndClearReferences } from '../../redux/user/workspaces/workspacesActionCreators';
+import { deleteRequest } from '../../api/workspace';
 
-const WorkspaceItem = ({ workspaceName, workspaceId, workspaceComponentsCount }) => {
-  const [selected, setSelected] = useState(false);
+const WorkspaceItem = ({ workspaceName, workspaceId, workspaceComponentsCount, email, token }) => {
   return (
-    <Link href={`/workspace/${workspaceId}`}>
-      <Card
-        fluid
-        id={workspaceId}
-        style={{
-          padding: '0.5em 0.5em 0.5em 0.5em',
-          backgroundColor: selected ? '#EEEEEE' : 'transparent'
-        }}
-        onMouseEnter={() => setSelected(true)}
-        onMouseLeave={() => setSelected(false)}>
-        <Card.Content>
-          <Card.Header>{workspaceName}</Card.Header>
-          <Card.Meta>{workspaceComponentsCount} Components</Card.Meta>
-        </Card.Content>
-      </Card>
-    </Link>
+    <Card fluid id={workspaceId}>
+      <Card.Content>
+        <Card.Header>{workspaceName}</Card.Header>
+        <Card.Meta>{workspaceComponentsCount} Components</Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <Link href={`/workspace/${workspaceId}`}>
+          <Button floated="left">
+            Enter
+            <Icon name="arrow right" />
+          </Button>
+        </Link>
+        <DeleteButton
+          floated="right"
+          deleteRequest={deleteRequest}
+          deleteRequestArgs={[{ email: email, token: token }, workspaceId]}
+          actionCreator={deleteWorkspaceAndClearReferences}
+          actionCreatorArgs={[workspaceId]}
+          resourceName="Workspace"
+        />
+      </Card.Content>
+    </Card>
   );
 };
 export default React.memo(WorkspaceItem);
