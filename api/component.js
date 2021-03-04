6c2a78fabc44eaca2df3312a1112be86c1f557a5
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-03 17:38:58 -0800

    added api wrapped for component post req

--- api/component.js
+++ api/component.js
@@ -1,3 +1,4 @@
+/** component.js contains wrapper functions for different requests*/
 import axios from 'axios';
 
 /**
@@ -15,6 +16,31 @@ export const deleteRequest = async (cookies, componentId, workspaceId) => {
       baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
       url: `/users/${email}/workspaces/${workspaceId}/components/${componentId}?_token=${token}`
     });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+/**
+ * @param {Object} cookies
+ * @param {String} cookies.email
+ * @param {String} cookies.token
+ * @param {String} workspaceId
+ * @param {String} questionId
+ */
+export const postRequest = async (cookies, workspaceId, questionId) => {
+  try {
+    const { email, token } = cookies;
+    const response = await axios({
+      method: 'POST',
+      baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+      url: `/users/${email}/workspaces/${workspaceId}/components?_token=${token}`,
+      data: {
+        answer: '',
+        questionId
+      }
+    });
+    return response;
   } catch (err) {
     throw new Error(err.message);
   }
