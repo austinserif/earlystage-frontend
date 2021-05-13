73c7989ddf57ba5c008503e2d94ffda8b7a0d4a1
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-12 19:15:40 -0700

    updated next.config.js to include custom target

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([[withImages]]);
+/**
+ * next-compose-plugins takes an array of plugins/loaders, and a
+ * global configuration object.
+ *
+ * ```
+ * withPlugins(
+ *  [
+ *      [withPlugin1],
+ *      [withPlugin2]
+ *  ],
+ * {
+ *      target: 'server',
+ *      /** ... any other global config files /
+ * }
+ * );
+ * ```
+ */
+
+module.exports = withPlugins([[withImages]], {
+  target: 'serverless'
+});
